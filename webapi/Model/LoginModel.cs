f135b830b1e659275f44b040386936f4c76476cd
chih-chien Hsiao(Daniel) <daniel13520cs@gmail.com>
2023-11-27 15:13:51 +0800

    update gitignore-

--- webapi/Model/LoginModel.cs
+++ webapi/Model/LoginModel.cs
@@ -1,5 +1,9 @@
 public class LoginModel
 {
+    public LoginModel(string username,  string password) {
+        this.Username = username;
+        this.Password = password;
+    }
     public string Username { get; set; }
     public string Password { get; set; }
 }
